d719994d621d7f9c760f47651569df19c73ddf75
Nick Norrell <norrell.nick@gmail.com>
2022-09-02 12:20:39 -0500

    Bump version

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,23 @@ module.exports = {
     content: ["./src/**/*.{js,ts,jsx,tsx}"],
     plugins: [require("daisyui")],
     daisyui: {
-        themes: true,
-        // themes: [
-        //     {
-        //         visualizer: {
-        //             "primary": "#F7E1D8",
-        //             "secondary": "#D7AFFE",
-        //             "accent": "#533CA6",
-        //             "neutral": "#191D24",
-        //             "base-100": "#232B3F",
-        //             "info": "#3ABFF8",
-        //             "success": "#36D399",
-        //             "warning": "#FBBD23",
-        //             "error": "#F87272",
-        //         },
-        //     },
-        // ],
+        themes: [
+            "light",
+            "dark",
+            "cyberpunk",
+            "corporate",
+            "cupcake",
+            "business",
+            "bumblebee",
+            "dracula",
+            "halloween",
+            "winter",
+            "lofi",
+            "emerald",
+            "synthwave",
+            "fantasy",
+            "night"
+        ],
     }
 };
 
